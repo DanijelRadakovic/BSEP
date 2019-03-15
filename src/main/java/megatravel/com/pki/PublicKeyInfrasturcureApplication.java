15fbce8902c40c7e6ae8b473f697a50aa8653204
DanijelRadakovic <danijelradakovic1996@gmail.com>
2019-03-15 17:45:05 +0100

    Created Spring project

---
+++ src/main/java/megatravel/com/pki/PublicKeyInfrasturcureApplication.java
@@ -0,0 +1,13 @@
+package megatravel.com.pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicKeyInfrasturcureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicKeyInfrasturcureApplication.class, args);
+	}
+
+}
